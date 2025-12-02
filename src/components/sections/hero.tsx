6c9e93ba2d7afe20d042fec58b329ab1eed82293
Vimaldharshan V <vimal-si3313@vimal-si3313.csez.zohocorpin.com>
2025-12-02 15:43:11 +0530

    resume update

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -83,7 +83,7 @@ const HeroSection = () => {
               <div className="mt-8 md:ml-2 flex flex-col gap-3">
                 <Link
                   href={
-                    "https://drive.google.com/file/d/1O97WCk2DrO9x6SHOqf7LvRbmHkMgGIb4/view?usp=sharing"
+                    "https://drive.google.com/file/d/1YIdS-C9BHsusGVj3ZVRR4KfJMQVVr5gG/view?usp=drive_link"
                   }
                   target="_blank"
                   className="flex-1"
