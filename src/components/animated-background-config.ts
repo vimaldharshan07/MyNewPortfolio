bd8e56697e6847463899ed0c7398ad5add229078
Vimaldharshan V <vimal-si3313@vimal-si3313.csez.zohocorpin.com>
2025-12-02 15:27:20 +0530

    first commit

---
+++ src/components/animated-background-config.ts
@@ -0,0 +1,116 @@
+export type Section = "hero" | "about" | "skills" | "projects" | "contact";
+
+export const STATES = {
+  hero: {
+    desktop: {
+      scale: { x: 0.3, y: 0.3, z: 0.3 },
+      position: { x: 400, y: -200, z: 0 },
+      rotation: { x: 0, y: 0, z: 0 },
+    },
+    mobile: {
+      scale: { x: 0.15, y: 0.15, z: 0.15 },
+      position: { x: 0, y: -200, z: 0 },
+      rotation: { x: 0, y: 0, z: 0 },
+    },
+  },
+  about: {
+    desktop: {
+      scale: { x: 0.4, y: 0.4, z: 0.4 },
+      position: { x: 0, y: -40, z: 0 },
+      rotation: {
+        x: 0,
+        y: Math.PI / 12,
+        z: 0,
+      },
+    },
+    mobile: {
+      scale: { x: 0.2, y: 0.2, z: 0.2 },
+      position: { x: 0, y: -40, z: 0 },
+      rotation: {
+        x: 0,
+        y: Math.PI / 6,
+        z: 0,
+      },
+    },
+  },
+  skills: {
+    desktop: {
+      scale: { x: 0.4, y: 0.4, z: 0.4 },
+      position: { x: 0, y: -40, z: 0 },
+      rotation: {
+        x: 0,
+        y: Math.PI / 12,
+        z: 0,
+      },
+    },
+    mobile: {
+      scale: { x: 0.2, y: 0.2, z: 0.2 },
+      position: { x: 0, y: -40, z: 0 },
+      rotation: {
+        x: 0,
+        y: Math.PI / 6,
+        z: 0,
+      },
+    },
+  },
+  projects: {
+    desktop: {
+      scale: { x: 0.3, y: 0.3, z: 0.3 },
+      position: { x: 0, y: -40, z: 0 },
+      rotation: {
+        x: Math.PI,
+        y: Math.PI / 3,
+        z: Math.PI,
+      },
+    },
+    mobile: {
+      scale: { x: 0.18, y: 0.18, z: 0.18 },
+      position: { x: 0, y: 150, z: 0 },
+      rotation: {
+        x: Math.PI,
+        y: Math.PI / 3,
+        z: Math.PI,
+      },
+    },
+  },
+  contact: {
+    desktop: {
+      scale: { x: 0.3, y: 0.3, z: 0.3 },
+      position: { x: 500, y: -250, z: 0 },
+      rotation: {
+        x: 0,
+        y: 0,
+        z: 0,
+      },
+    },
+    mobile: {
+      scale: { x: 0.18, y: 0.18, z: 0.18 },
+      position: { x: 0, y: 150, z: 0 },
+      rotation: {
+        x: Math.PI,
+        y: Math.PI / 3,
+        z: Math.PI,
+      },
+    },
+  },
+};
+
+export const getKeyboardState = ({
+  section,
+  isMobile,
+}: {
+  section: Section;
+  isMobile: boolean;
+}) => {
+  const baseTransform = STATES[section][isMobile ? "mobile" : "desktop"];
+  const scaleOffset = +(window.devicePixelRatio - 0.4).toFixed(1)
+
+  return {
+    ...baseTransform,
+    scale: {
+      x: Math.abs(baseTransform.scale.x / scaleOffset),
+      y: Math.abs(baseTransform.scale.y / scaleOffset),
+      z: Math.abs(baseTransform.scale.z / scaleOffset),
+    },
+  };
+};
